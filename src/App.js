d8c30cc65ffd1d93723e1e6c14c27741b5a62101
andytle95 <48510312+andytle95@users.noreply.github.com>
2022-12-27 12:10:25 -0800

    changed file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './components/Navbar/Navbar'
+
 const App = () => {
   return (
     <div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
-import { MdOutlineRestaurantMenu } from 'react-icons/md'
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
-import images from '../../img'
-import './Navbar.css';
+// import images from '../../img'
+import './components/Navbar/Navbar.css';
 
 const Navbar = () => (
     <nav className="app__navbar">
         <div classname="app__navbar-logo">
-            <img src={images.anam} alt="app logo" />
+            {/* <img src={images.anam} alt="app logo" /> */}
         </div>
         <ul className='app__navbar-links'>
             <li className='p__opensans'><a href="#home">Home</a></li>
