3ae5b2750c07227efe6e0f1065a65a7fbc6706af
Andy Le <andytle95@gmail.com>
2022-12-31 12:28:09 -0800

    Header finished, about us started

--- anamsite/src/container/Header/Header.jsx
+++ anamsite/src/container/Header/Header.jsx
@@ -4,14 +4,16 @@ import { SubHeading } from '../../components'
 import { images } from '../../constants';
 import './Header.css';
 
-const Header = () => (
+const Header = (props) => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading />
+      <SubHeading title="Chase the new flavor!"/>
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{ margin: '2rem 0' }}>generic paragraph</p>
+      <button type="button" className="custom__button">Explore More</button>
     </div>
     <div className='app__wrapper_img'>
-
+      <img src={images.welcome} alt="header img" />
     </div>
   </div>
 );
