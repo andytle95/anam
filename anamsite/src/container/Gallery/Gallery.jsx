42971a591658ba398798ce625a411105b9a4f6d4
Andy Le <andytle95@gmail.com>
2023-01-04 21:15:30 -0800

    gallery started

--- anamsite/src/container/Gallery/Gallery.jsx
+++ anamsite/src/container/Gallery/Gallery.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
 
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
+
+const Gallery = () => {
+  const scrollRef = React.useRef(null);
+
+  return (
+  <div className='app__gallery flex__center'>
+    <div className='app__gallery-content'>
+      <SubHeading title="Instagram" />
+      <h1 className='headtext__cormorant'>Photo Gallery</h1>
+      <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem' }}>LOL wow fr</p>
+      <button type="button" className='custom__button'>View More</button>
+    </div>
+
+    <div className='app__gallery-images'>
+      <div className='app__gallery-images_container' ref={scrollRef}>
+
+      </div>
+      <div className='app__gallery-images_arrow'>
+        <BsArrowLeftShort className='gallery__arrow-icons'></BsArrowLeftShort>
+      </div>
+    </div>
   </div>
 );
+}
 
 export default Gallery;
