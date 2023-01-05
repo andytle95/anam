6517de22b5a365adb2fe1051a8c611f3d44b3cb2
Andy Le <andytle95@gmail.com>
2023-01-04 22:28:23 -0800

    gallery broke whole website, need to fix

--- anamsite/src/container/Gallery/Gallery.jsx
+++ anamsite/src/container/Gallery/Gallery.jsx
@@ -5,9 +5,20 @@ import { SubHeading } from '../../components'
 import { images } from '../../constants'
 import './Gallery.css';
 
+const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04]
+
 
 const Gallery = () => {
   const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300
+    }
+  }
 
   return (
   <div className='app__gallery flex__center'>
@@ -20,10 +31,16 @@ const Gallery = () => {
 
     <div className='app__gallery-images'>
       <div className='app__gallery-images_container' ref={scrollRef}>
-
+        {images.map((image, index) => (
+          <div className="app__gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
+            <img src={image} alt="gallery" />
+            <BsInstagram className="gallery__image-icon" />
+            </div>
+        ))};
       </div>
       <div className='app__gallery-images_arrow'>
-        <BsArrowLeftShort className='gallery__arrow-icons'></BsArrowLeftShort>
+        <BsArrowLeftShort className='gallery__arrow-icons' onClick={() => scroll('left')}/>
+        <BsArrowRightShort className='gallery__arrow-icons' onClick={() => scroll('left')} />
       </div>
     </div>
   </div>
